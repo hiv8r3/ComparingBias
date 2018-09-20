74c02e754a73c339f310f55319e2f0f7892bd514
hiv8r3 <hannah.volpert@mail.missouri.edu>
2018-09-19 21:38:25 -0400

    Including IMS in main analysis doc for submission #4

--- Data and Analyses/8 Supplementary analyses.R
+++ Data and Analyses/8 Supplementary analyses.R
@@ -219,3 +219,130 @@ s2.test$Task = factor(s2.test$Task)
 
 aov(DiffA ~ Observer*Task + Error(Subject/(Task)), data = s2.test) %>% 
   summary()
+
+
+
+
+# 3. Anxiety stuff --------------------------------------------------------
+
+anxDat = read.delim("Study2_questDat.txt")
+qplot(x=Var1, y=Var2, data=melt(cor(filter(anxDat, Subject != 87)[c(3:6, 8:18)])), fill=value, geom="tile") # sub 87 doesn't have data for the last four questions
+
+hist(anxDat$Anx_composite, main = "Histogram for anxiety composite")
+
+
+anxDat$Task = "APT"
+anxDat$Task[grep("WIT", anxDat$blockName)] = "WIT"
+
+block1 = anxDat[grep(1, anxDat$blockName),]
+block2 = anxDat[grep(2, anxDat$blockName),]
+
+block1 = select(block1, Subject, Task, Anx_composite) %>% 
+  rename(Anx_composite1 = Anx_composite)
+block2 = select(block2, Subject, Task, Anx_composite) %>% 
+  rename(Anx_composite2 = Anx_composite)
+
+sepBlock = cbind(block1, select(block2, Anx_composite2))
+
+ggplot(sepBlock, aes(Anx_composite1, Anx_composite2, color = Task)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+  geom_smooth(method="lm") +
+  labs(x = "Block 1", y = "Block 2")
+
+# have to take sub 87 out for correlation, has missing data
+
+
+# create error-related distress
+questDat = read.delim("Study1_questDat.txt")
+# Add post-task questions
+questDat$blockName = as.character(questDat$blockName)
+questDat$blockName[questDat$blockName == "PostWITquestions"] = "WIT"
+questDat$blockName[questDat$blockName == "PostAPquestions"] = "APT"
+questDat$blockName = as.factor(questDat$blockName)
+
+names(questDat)[3] = "Resp"
+
+dat = data.frame(Subject = rep(unique(questDat$Subject), each = 2),
+                 blockName = rep(c("WIT", "APT"), length(unique(questDat$Subject))))
+for (i in unique(questDat$Subject)) {
+  for (j in c("WIT", "APT")) {
+    dat$Frust[dat$Subject == i & dat$blockName == j] = questDat$Resp[questDat$Subject == i & 
+                                                                       questDat$SubTrial == 1 &
+                                                                       questDat$blockName == j]
+    dat$Anx[dat$Subject == i & dat$blockName == j] = questDat$Resp[questDat$Subject == i & 
+                                                                     questDat$SubTrial == 2 &
+                                                                     questDat$blockName == j]
+    dat$Unpleas[dat$Subject == i & dat$blockName == j] = questDat$Resp[questDat$Subject == i & 
+                                                                         questDat$SubTrial == 3 &
+                                                                         questDat$blockName == j]
+    dat$Attend[dat$Subject == i & dat$blockName == j] = questDat$Resp[questDat$Subject == i & 
+                                                                        questDat$SubTrial == 4 &
+                                                                        questDat$blockName == j]
+    dat$Effort[dat$Subject == i & dat$blockName == j] = questDat$Resp[questDat$Subject == i & 
+                                                                        questDat$SubTrial == 5 &
+                                                                        questDat$blockName == j]
+  }
+}
+
+dat$Err_comp = (dat$Frust + dat$Anx + dat$Unpleas)/3
+
+#Moderation of observer effect by anxiety:**
+#  Uses a multiple regression because of continuous nature of anxiety.
+
+  lm(perfBias ~ Observer*Anx_composite, data = s2.wide) %>% 
+  summary()
+
+lm(MeanC ~ Observer*Anx_composite, data = s2.wide) %>% 
+  summary()
+
+lm(AResid ~ Observer*Anx_composite, data = s2.wide) %>% 
+  summary()
+
+
+### 6. Look at relationship between anxiety and PDP-A/PDP-C
+#Anxiety composite scores were averaged across blocks, so each participant has one anxiety score per task.
+
+
+ggplot(s2.wide, aes(scale(Anx_composite), MeanC, color = Task, shape = Task)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  scale_color_manual(values=c("black", "forestgreen")) +
+  scale_shape_manual(values=c(1,2)) +
+  labs(x="Anxiety composite score", y = "PDP-C") +
+  #  ggtitle("Anxiety predicting PDP-C") +  
+  theme_bw() +
+  theme(panel.grid.major = element_line(color = "white"),
+        panel.grid.minor = element_line(color = "white"),
+        strip.text.x = element_text(face = "bold", size = 14),
+        strip.background = element_rect(fill = "grey98"),
+        axis.text.x = element_text(size = 16),
+        axis.text.y = element_text(size = 16),
+        axis.title.x = element_text(size = 20),
+        axis.title.y = element_text(size = 20))
+ggsave("./Figures/AnxPredictingPDPC.tiff")
+
+lmer(MeanC ~ scale(Anx_composite)*Task + (1|Subject), data = s2.wide) %>% summary()
+
+ggplot(s2.wide, aes(scale(Anx_composite), AResid, color = Task, shape = Task)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  scale_color_manual(values=c("black", "forestgreen")) +
+  scale_shape_manual(values=c(1,2)) +
+  labs(x="Anxiety composite score", y = "PDP-A") +
+  #  ggtitle("Anxiety predicting PDP-A") +  
+  theme_bw() +
+  theme(panel.grid.major = element_line(color = "white"),
+        panel.grid.minor = element_line(color = "white"),
+        strip.text.x = element_text(face = "bold", size = 14),
+        strip.background = element_rect(fill = "grey98"),
+        axis.text.x = element_text(size = 16),
+        axis.text.y = element_text(size = 16),
+        axis.title.x = element_text(size = 20),
+        axis.title.y = element_text(size = 20))
+ggsave("./Figures/AnxPredictingPDPA.tiff")
+
+lmer(AResid ~ scale(Anx_composite)*Task + (1|Subject), data = s2.wide) %>% summary()
+
+
+
